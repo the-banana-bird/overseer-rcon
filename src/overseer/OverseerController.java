f52f8d35f09444a2a818f3180a642bcfaa3c3707
Kevin <kevin@foxusa.net>
2020-03-28 14:59:15 -0700

    Moved main to controller, support dynamic server list

--- src/overseer/OverseerController.java
+++ src/overseer/OverseerController.java
@@ -1,26 +1,45 @@
 package overseer;
 
+import java.awt.EventQueue;
 import java.util.ArrayList;
 import java.util.List;
 
 import overseer.rcon.RconSession;
+import overseer.ui.OverseerFrame;
 import overseer.view.OverseerServerViewModel;
 import overseer.view.OverseerViewModel;
 
 public class OverseerController {
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		// Get main controller instance started
+		OverseerController controller = new OverseerController();
+
+		// Prepare view model for main window
+		OverseerViewModel viewModel = controller.viewOverseer();
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					OverseerFrame frame = new OverseerFrame(viewModel);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
 	public OverseerViewModel viewOverseer() {
 		List<OverseerServerViewModel> servers = new ArrayList<OverseerServerViewModel>();
 
-		for (int i = 0; i < 5; i++) {
-			servers.add(viewMockServer());
-		}
-
 		return new OverseerViewModel("Overseer RCON", "1.0.0", servers);
 	}
 
-	public OverseerServerViewModel viewMockServer() {
-		return new OverseerServerViewModel("", new RconSession("", 0, ""));
+	public OverseerServerViewModel viewMockServer(String serverName, String ip, int port, String password) {
+		return new OverseerServerViewModel(serverName, new RconSession(ip, port, password));
 	}
 
 }
--- src/overseer/ui/OverseerFrame.java
+++ src/overseer/ui/OverseerFrame.java
@@ -1,59 +1,39 @@
 package overseer.ui;
 
 import java.awt.Dimension;
-import java.awt.EventQueue;
+import java.awt.GridLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.SpringLayout;
 import javax.swing.border.EmptyBorder;
 
-import overseer.OverseerController;
+import overseer.view.OverseerServerViewModel;
+import overseer.view.OverseerViewModel;
 
 public class OverseerFrame extends JFrame {
 
+	private OverseerViewModel viewModel;
 	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static OverseerController controller;
-
-	public static void main(String[] args) {
-		controller = new OverseerController();
-
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					OverseerFrame frame = new OverseerFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the frame.
 	 */
-	public OverseerFrame() {
-		setMinimumSize(new Dimension(600, 250));
-		setTitle("OverseerRCON - 1.0.0");
+	public OverseerFrame(OverseerViewModel viewModel) {
+		this.viewModel = viewModel;
+
+		setMinimumSize(new Dimension(600, 1000));
+		setTitle(String.format("%s - %s", this.viewModel.title, this.viewModel.version));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 600, 250);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		SpringLayout sl_contentPane = new SpringLayout();
-		contentPane.setLayout(sl_contentPane);
+		contentPane.setLayout(new GridLayout(0, 1, 0, 0));
 
-		OverseerServerPanel overseerServerPanel = new OverseerServerPanel(controller.viewMockServer());
-		sl_contentPane.putConstraint(SpringLayout.NORTH, overseerServerPanel, 0, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.WEST, overseerServerPanel, 0, SpringLayout.WEST, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, overseerServerPanel, 0, SpringLayout.SOUTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, overseerServerPanel, 0, SpringLayout.EAST, contentPane);
-		contentPane.add(overseerServerPanel);
+		for (OverseerServerViewModel serverViewModel : this.viewModel.servers) {
+			OverseerServerPanel overseerServerPanel = new OverseerServerPanel(serverViewModel);
+			contentPane.add(overseerServerPanel);
+		}
 
 	}
 }
